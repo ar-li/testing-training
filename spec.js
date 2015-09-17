8b7d9ba04e7edf5d601931cf59124c33f1236cdd
JanisDzalbe <dzalbe.janis@LVALTP0605.dir.svc.accenture.com>
2015-09-17 15:29:12 +0300

    Replaced “filter” and CSS selector with XPath. Works faster and returned object is not undefined anymore

--- spec.js
+++ spec.js
@@ -1,24 +1,16 @@
 // spec.js
 describe('Protractor Demo App', function() {
 	function MainPage () {
-		this.pageAdress = 'http://angular.github.io/angular-phonecat/step-12/app/#/phones';
 		this.getPhoneByName = function(name) {
-			var allPhones = element.all(by.repeater('phone in phones | filter:query | orderBy:orderProp'))
-			.all(by.css('a.ng-binding'));
-			allPhones.filter(function(elem, index) {
-				return elem.getText().then(function(text) {
-					return text === name;
-				});
-			}).then(function(filteredElements) {
-					filteredElements[0].click();
-					return new PhonePage;
-			});
+			var selectedPhone = browser.findElement(By.xpath('//ul[@class=\'phones\']/li/a[text()=\''+name+'\']'));
+			selectedPhone.click();
+			return new PhonePage;
 		};
 	};
 	
 	function PhonePage () {
 		this.getName = function(){
-			$('h1.ng-binding')
+			return $('h1.ng-binding');
 		};
 		this.getPic = function() {
 			console.log('Nothing here');
@@ -26,25 +18,23 @@ describe('Protractor Demo App', function() {
 	};
 
 	
-  it('should have a title', function() {
-    browser.get('http://angular.github.io/angular-phonecat/step-12/app/#/phones');
+	it('should have a title', function() {
+		browser.get('http://angular.github.io/angular-phonecat/step-12/app/#/phones');
 
-    expect(browser.getTitle()).toEqual('Google Phone Gallery');
-  });
+		expect(browser.getTitle()).toEqual('Google Phone Gallery');
+	});
   
-  it('should find phones', function(){
-	  //var phones = $$('li, .thumbnail phone-listing ng-scope');
-	  var phones = element.all(by.repeater('phone in phones | filter:query | orderBy:orderProp'));
-	  expect(phones.count()).toEqual(20);
-  })
-  it('should click on phone', function(){
-	  var testPhoneName = 'Dell Streak 7'
-	  var tmpPage = new MainPage;
-	  //tmpPage.getPhoneByName(testPhoneName);
-	  
-	  //$('h1.ng-binding').getText().then(function(text){console.log(text)});
-	  //expect($('h1.ng-binding').getText()).toEqual(testPhoneName);
-	  var tmp = tmpPage.getPhoneByName(testPhoneName);
-	  tmp.getName.getText().then(function(text){console.log(text)});
-  })
+	it('should find phones', function(){
+		//var phones = $$('li, .thumbnail phone-listing ng-scope');
+		var phones = element.all(by.repeater('phone in phones | filter:query | orderBy:orderProp'));
+		expect(phones.count()).toEqual(20);
+	})
+	it('should click on phone', function(){
+		var testPhoneName = 'Dell Streak 7'
+		var tmpPage = new MainPage;
+		
+		var tmp = tmpPage.getPhoneByName(testPhoneName);
+		//tmp.getName().getText().then(function(text){console.log(text)});
+		expect(tmp.getName().getText()).toEqual(testPhoneName);
+	})
 });
